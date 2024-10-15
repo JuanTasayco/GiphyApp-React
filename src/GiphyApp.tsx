ed271947b7c38a1d40ea32af576f5b908e90ffc1
JuanTasayco <juantasayco266@gmail.com>
2024-10-15 02:39:12 -0500

    feat: add new section delete

--- src/GiphyApp.tsx
+++ src/GiphyApp.tsx
@@ -11,14 +11,28 @@ function GiphyApp() {
     }
   };
 
+  const deleteSection = (nameCategorie: string) => {
+    setCategories((categories) => {
+      return categories.filter((cat) => cat !== nameCategorie);
+    });
+  };
+
   return (
     <div className="container mt-5">
       <InputSearch onNewCategory={onAddCategory}></InputSearch>
 
       {categories.map((name, index) => {
         return (
-          <div className={index === 0 ? "mt-1" : "mt-4"} key={index}>
-            <h1>{capitalize(name)}</h1>
+          <div className={index === 0 ? "mt-3" : "mt-5"} key={index}>
+            <div className="d-flex align-items-center justify-content-between">
+              <h1>{capitalize(name)}</h1>
+              <button
+                type="button"
+                className="btn-close"
+                aria-label="Close"
+                onClick={() => deleteSection(name)}
+              ></button>
+            </div>
             <CardGiphy currentSearch={name}></CardGiphy>
           </div>
         );
